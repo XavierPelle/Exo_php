8e5e9cefec30affe79087f925bc57366265401e6
Xavier Pellé <114385560+XavierPelle@users.noreply.github.com>
2023-04-10 22:46:15 +0200

    Update user.php

--- src/datas/user.php
+++ src/datas/user.php
@@ -1,12 +1,10 @@
 <?php
 $file = dirname(__FILE__)."/users.csv";
-echo basename(dirname($_SERVER['SCRIPT_NAME']), "/");
 $tab = [];
-
 
 if (file_exists($file)) {
 
-    $content = file($file) or die("ERROR: Cannot open the file.");
+    $content = file($file);
     foreach ($content as $line) {
         $info = explode(",", $line);
         $tab[$info[0]] = [
@@ -15,6 +13,4 @@ if (file_exists($file)) {
             'nom' => $info[3]
         ];
     }
-} else {
-    echo "ERROR: File does not exist.";
 }
